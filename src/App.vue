1850e1d33c4040e7de5b4d4725fd6d424c28e808
Ramonprog <programador.ramon@gmail.com>
2023-11-18 12:31:54 -0300

    feat: componente de backgoround animado

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import BackgroundComponent from "./component/Background.vue";
 </script>
 
+<template>
+  <BackgroundComponent>
+    <div class="container">
+      <div class="div">
+        <input type="text" />
+      </div>
+    </div>
+  </BackgroundComponent>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container {
+  width: 100%;
+  height: 100dvh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.div {
+  width: 200px;
+  height: 100px;
+  background-color: red;
+  z-index: 1;
 }
 </style>
