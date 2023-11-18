c3bb9440dc70e15f245d82365f02f952ce57a377
Ramonprog <programador.ramon@gmail.com>
2023-11-18 20:45:13 -0300

    feat: create a Results and Answers components

---
+++ src/component/TriviaAnswers.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  correctAnswers: Array
+})
+</script>
+
+<template>
+  <p v-if="correctAnswers.length">
+    <span>Respostas certas:</span>
+    <ul>
+      <li v-for="answer in correctAnswers">{{ answer }}</li>
+    </ul>
+  </p>
+</template>
+
+<style scoped>
+p {
+  text-align: left;
+}
+
+span {
+  font-weight: bold;
+}
+
+li{
+    list-style: none;
+}
+
+span{
+    margin-top: 20px;
+}
+</style>
---
+++ src/component/TriviaResults.vue
@@ -0,0 +1,31 @@
+<script setup>
+import TriviaButtons from "./TriviaButtons.vue";
+
+defineProps({
+  correctAnswersTotal: Number,
+  questionsTotal: Number,
+});
+
+const reload = () => {
+  window.location.reload();
+};
+</script>
+
+<template>
+  <div class="container">
+    <p>
+      Você acertou {{ correctAnswersTotal }} de {{ questionsTotal }} perguntas!
+    </p>
+
+    <TriviaButtons @click="reload">Jogar de novo</TriviaButtons>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  max-height: 100px;
+}
+</style>
